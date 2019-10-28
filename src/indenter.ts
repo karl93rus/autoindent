3172f3d5c8aeaa823f508f7656118df26a447a5d
Aleksandr Sazonov <aleksandr.sazonov@south.rt.ru>
2019-10-28 15:31:20 +0300

    ignore

--- src/indenter.ts
+++ src/indenter.ts
@@ -1,5 +1,37 @@
 let currentMaxIdent: number = 0;
 
+export function indent2(arr: string[]): string[] {
+	let maxIndent: number = 0;
+	let res: string[] = [];
+	for(let i = 0; i < arr.length; i++) {
+		let separateKey = arr[i].split(':');
+		if(separateKey[0].length > maxIndent) {
+			maxIndent = separateKey[0].length;
+			separateKey.join(':');
+		}
+	}
+	console.log('current MAX indent', currentMaxIdent, 'max indent', maxIndent);
+	if(maxIndent <= currentMaxIdent) {
+		console.log('returning []', currentMaxIdent);
+		return [] as string[];
+	} else if(maxIndent > currentMaxIdent) {
+		currentMaxIdent = maxIndent;
+		console.log('current MAX indent', currentMaxIdent, 'max indent', maxIndent);
+		for(let i = 0; i < arr.length; i++) {
+			let separateKey: string[] = arr[i].split(':');
+			let spacesToAdd: number = maxIndent - separateKey[0].length;
+			let spaces: string = '';
+			for(let i = 0; i < spacesToAdd; i++) {
+				spaces += ' ';
+			}
+			separateKey[1] = spaces + separateKey[1];
+			res.push(separateKey.join(':'));
+		}
+	}
+	console.log('indent NEW res returns');
+	return res;
+}
+
 export function indent(arr: string[]): string[] {
 	let maxIndent: number = 0;
 	let res: string[] = [];
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import fs from 'fs';
 import { replace } from './replacer';
 import { readByLine } from './readByLine';
 import { parseFile } from './parseFile';
-import { indent } from './indenter';
+import { indent2 } from './indenter';
 
 let fileContent: string = fs.readFileSync(`${__dirname}/../test/test_json.ts`, 'utf8');
 let strings: string[] = readByLine(fileContent);
 
 let parsedFile = parseFile(strings);
 
-let done = replace(strings, indent(parsedFile)).join('');
+let done = replace(strings, indent2(parsedFile)).join('');
 
 fs.writeFileSync(`${__dirname}/../test/test_json.ts`, done, 'utf8');
