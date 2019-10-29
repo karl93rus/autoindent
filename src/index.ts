e5c31d137f06e4b72320d6d2e376326985bf5a1d
Aleksandr Sazonov <aleksandr.sazonov@south.rt.ru>
2019-10-29 08:32:50 +0300

    delete unused imports

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import fs from 'fs';
 import { replace } from './replacer';
 import { readByLine } from './readByLine';
 import { parseFile } from './parseFile';
-import { indent2 } from './indenter';
 
 let fileContent: string = fs.readFileSync(`${__dirname}/../test/test_json.ts`, 'utf8');
 let strings: string[] = readByLine(fileContent);
